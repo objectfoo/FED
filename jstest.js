e411b7db726cb914d554641c71d662fdb4d78f92
Satake <satake@objectfoo.com>
2013-08-28 13:13:07 -0700

    don't rely one external js file

--- jstest.js
+++
@@ -1,7 +0,0 @@
-// Write the counter function so that the
-// current value is displayed in the label
-// and the buttons change the value
-// The counter should have this API:
-//      counter.incr();
-//      counter.decr();
-//      counter.value;
