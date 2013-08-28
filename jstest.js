e309357968353e1cb0db12f415e42b8a86b955d5
Satake <satake@objectfoo.com>
2013-08-27 20:41:55 -0700

    initial commit

---
+++ jstest.js
@@ -0,0 +1,7 @@
+// Write the counter function so that the
+// current value is displayed in the label
+// and the buttons change the value
+// The counter should have this API:
+//      counter.incr();
+//      counter.decr();
+//      counter.value;
